457e721d73c8387b3c8aae50bd32b32c52dac6fe
Thienle <wavelets11@gmail.com>
2023-10-24 19:47:58 +0700

    Add files via upload

---
+++ ai_eye.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 11 11:32:36 2021
+The multi-page web application for stroke project- Chula
+@author: Ngoc Thien Le, Postdoc Researcher
+"""
+import streamlit as st
+from PIL import Image
+
+logo_image = Image.open('D:\OneDrive\streamlitprojects\eyes_web_apps/logo.png')
+
+st.set_page_config(layout="centered", page_title='AI Eyecare',
+                   page_icon=logo_image)
+
+from multiapp import MultiApp
+from apps import introduction, fundus_detection  # Import the app modules want here.
+
+app = MultiApp()
+
+# Add all your application here
+app.add_app("Project Objectives", introduction.app)
+app.add_app("AMD examination", fundus_detection.app)
+
+# The main apps
+app.run()
